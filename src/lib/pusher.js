d067af00fbd1b4f0fcfc686e87cead79cd80a304
paul <nutter20912@gmail.com>
2023-05-09 22:34:50 +0800

    關閉連接pusher

--- src/lib/pusher.js
+++ src/lib/pusher.js
@@ -6,7 +6,11 @@ window.Echo = new Echo({
   broadcaster: process.env.REACT_APP_PUSHER_BROADCASTER || '',
   key: process.env.REACT_APP_PUSHER_KEY || '',
   cluster: process.env.REACT_APP_PUSHER_CLUSTER || '',
-  forceTLS: true,
+  // forceTLS: true,
+  wsHost: window.location.hostname,
+  wsPort: 6001,
+  forceTLS: false,
+  disableStats: true,
 });
 
 export default window.Echo;
--- src/pages/baseLayout.jsx
+++ src/pages/baseLayout.jsx
@@ -75,13 +75,17 @@ export default function BaseLayout({ menuComponents }) {
 
   /** 使用者推播 */
   useEffect(() => {
-    pusher().then((client) => {
-      client.private(`users.${user?.id}`)
-        .listen('.CommentCreated', ({ model }) => openNotification(model))
-        .listen('UserRelationshipCreated', (payload) => handleUserRelationshipCreated(payload))
-        .listen('RelationshipConfirmed', (payload) => console.log(payload))
-        .error((error) => message.error(error));
-    });
+    const test = () => {
+      pusher().then((client) => {
+        client.private(`users.${user?.id}`)
+          .listen('.CommentCreated', ({ model }) => openNotification(model))
+          .listen('UserRelationshipCreated', (payload) => handleUserRelationshipCreated(payload))
+          .listen('RelationshipConfirmed', (payload) => console.log(payload))
+          .error((error) => message.error(error));
+      });
+    };
+
+    console.log(test);
   }, []);
 
   return (
